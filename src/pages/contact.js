9944e9d329a334777c43715f06a11821dd4cd55d
Shreyzeous21 <shrey.sadhukhan21@gmail.com>
2024-10-23 18:16:35 +0530

    done

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -164,7 +164,7 @@ function Contact() {
           <div className="map-section pt-120 pb-0 overflow-hidden">
             <div className="container-fluid px-0">
               <iframe
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d48330.162702269045!2d-74.29798882771155!3d40.792034138683825!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c3ab00d85ee855%3A0x93a15ba40269dd0!2sWest%20Orange%2C%20NJ%2007052%2C%20USA!5e0!3m2!1sen!2sbd!4v1658243800106!5m2!1sen!2sbd"
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d218.909859496189!2d77.36706605047073!3d28.61304129533933!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390ce57e4c8df143%3A0x3e7993a6c3056c48!2sBhutani%20Cyberpark!5e0!3m2!1sen!2sin!4v1729687552923!5m2!1sen!2sin"
                 height={500}
                 style={{ border: 0 }}
                 allowFullScreen
