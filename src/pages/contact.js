c402418d471ca54bc7092aaa0b69e61adf7eb515
Shreyzeous21 <shrey.sadhukhan21@gmail.com>
2024-10-28 17:31:47 +0530

    done

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,8 +1,13 @@
 import React from "react";
 import Breadcrumb from "../common/Breadcrumb";
 import Layout from "../components/layout/Layout";
+import { useForm, ValidationError } from '@formspree/react';
 
 function Contact() {
+  const [state, handleSubmit] = useForm("xzzbdjll");
+
+ 
+
   return (
     <>
       <Layout>
@@ -108,56 +113,81 @@ function Contact() {
                     </ul>
                   </div>
                 </div>
-                <div className="col-lg-6 col-md-6 text-md-start text-center">
-                  <div className="section-title2 mb-50">
-                    <h2>Have Any Questions</h2>
-                  </div>
-                  <form className="contact-form">
-                    <div className="row">
-                      <div className="col-lg-12">
-                        <div className="form-inner sibling2">
-                          <input type="text" placeholder="Enter your name" />
-                        </div>
-                      </div>
-                      <div className="col-lg-6">
-                        <div className="form-inner sibling2">
-                          <input type="email" placeholder="Enter your email" />
-                        </div>
-                      </div>
-                      <div className="col-lg-6">
-                        <div className="form-inner sibling2">
-                          <input type="text" placeholder="Enter your Phone" />
-                        </div>
-                      </div>
-                      <div className="col-lg-12">
-                        <div className="form-inner sibling2">
-                          <textarea
-                            rows={5}
-                            placeholder="Your message"
-                            defaultValue={""}
-                          />
-                        </div>
-                      </div>
-                      <div className="col-lg-12 text-lg-start text-center">
-                        <button
-                          type="submit"
-                          className="eg-btn btn--outline btn--lg sibling3 capsule"
-                        >
-                          Send Message &nbsp;
-                          <svg
-                            width={18}
-                            height={15}
-                            viewBox="0 0 22 13"
-                            xmlns="http://www.w3.org/2000/svg"
-                          >
-                            <path d="M21.9805 6.64708C21.955 6.74302 20.6834 7.78829 18.0766 9.85862C13.9311 13.156 14.0201 13.0954 13.5751 12.949C13.1809 12.8177 13.0219 12.5097 13.1809 12.1814C13.2127 12.1057 14.6369 10.9342 16.3408 9.5809L19.4309 7.11669V5.90479L16.3091 3.41534C14.23 1.75907 13.1682 0.885493 13.1427 0.789551C13.041 0.466377 13.2635 0.143203 13.6577 0.0472607C13.7595 0.0270623 13.8485 0.00181433 13.8612 0.00181433C14.0201 -0.0385824 14.8467 0.582518 18.1148 3.18306C20.6898 5.23824 21.955 6.27846 21.9805 6.36935C22.0059 6.45015 22.0059 6.57134 21.9805 6.64708Z" />
-                            <path d="M17.4313 5.90479V7.11669L2.71236 7.10659C2.27365 7.10608 1.84766 7.10558 1.43438 7.10507C1.19278 7.10507 0.954985 7.10457 0.721643 7.10457C0.320448 7.09396 0 6.83189 0 6.51074C0 6.34662 0.0839268 6.19817 0.218718 6.09061C0.349695 5.98659 0.528993 5.92044 0.728001 5.9169L1.23283 5.9164L2.706 5.91488L17.4313 5.90479Z" />
-                          </svg>
-                        </button>
-                      </div>
-                    </div>
-                  </form>
-                </div>
+                {/* form start */}
+                
+    <div className="col-lg-6 col-md-6 text-md-start text-center">
+      <div className="section-title2 mb-50">
+        <h2>Have Any Questions</h2>
+      </div>
+      <form className="contact-form" onSubmit={handleSubmit}>
+        <div className="row">
+          <div className="col-lg-12">
+            <div className="form-inner sibling2">
+              <input
+                type="text"
+                id="name"
+                name="name"
+                placeholder="Enter your name"
+                required
+              />
+              <ValidationError prefix="Name" field="name" errors={state.errors} />
+            </div>
+          </div>
+          <div className="col-lg-6">
+            <div className="form-inner sibling2">
+              <input
+                type="email"
+                id="email"
+                name="email"
+                placeholder="Enter your email"
+                required
+              />
+              <ValidationError prefix="Email" field="email" errors={state.errors} />
+            </div>
+          </div>
+          <div className="col-lg-6">
+            <div className="form-inner sibling2">
+              <input
+                type="text"
+                id="phone"
+                name="phone"
+                placeholder="Enter your Phone"
+              />
+              <ValidationError prefix="Phone" field="phone" errors={state.errors} />
+            </div>
+          </div>
+          <div className="col-lg-12">
+            <div className="form-inner sibling2">
+              <textarea
+                rows={5}
+                id="message"
+                name="message"
+                placeholder="Your message"
+                required
+              />
+              <ValidationError prefix="Message" field="message" errors={state.errors} />
+            </div>
+          </div>
+          <div className="col-lg-12 text-lg-start text-center">
+            <button
+              type="submit"
+              className="eg-btn btn--outline btn--lg sibling3 capsule"
+              disabled={state.submitting}
+            >
+              Send Message &nbsp;
+              <svg width={18} height={15} viewBox="0 0 22 13" xmlns="http://www.w3.org/2000/svg">
+                <path d="M21.9805 6.64708C21.955 6.74302 20.6834 7.78829 18.0766 9.85862C13.9311 13.156 14.0201 13.0954 13.5751 12.949C13.1809 12.8177 13.0219 12.5097 13.1809 12.1814C13.2127 12.1057 14.6369 10.9342 16.3408 9.5809L19.4309 7.11669V5.90479L16.3091 3.41534C14.23 1.75907 13.1682 0.885493 13.1427 0.789551C13.041 0.466377 13.2635 0.143203 13.6577 0.0472607C13.7595 0.0270623 13.8485 0.00181433 13.8612 0.00181433C14.0201 -0.0385824 14.8467 0.582518 18.1148 3.18306C20.6898 5.23824 21.955 6.27846 21.9805 6.36935C22.0059 6.45015 22.0059 6.57134 21.9805 6.64708Z" />
+                <path d="M17.4313 5.90479V7.11669L2.71236 7.10659C2.27365 7.10608 1.84766 7.10558 1.43438 7.10507C1.19278 7.10507 0.954985 7.10457 0.721643 7.10457C0.320448 7.09396 0 6.83189 0 6.51074C0 6.34662 0.0839268 6.19817 0.218718 6.09061C0.349695 5.98659 0.528993 5.92044 0.728001 5.9169L1.23283 5.9164L2.706 5.91488L17.4313 5.90479Z" />
+              </svg>
+            </button>
+            {state.succeeded && (
+              <p className="mt-3 text-success">Thanks for reaching out! We'll get back to you soon.</p>
+            )}
+          </div>
+        </div>
+      </form>
+    </div>
+                {/* form end */}
               </div>
             </div>
           </div>
